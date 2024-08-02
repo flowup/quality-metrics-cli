a1f4c5a6b7196e67332e584a3518271c5f9cd1a3
Michael <michael.hladky@push-based.io>
2024-08-03 00:37:43 +0200

    feat(create-cli): package to align with package managers init command

--- packages/nx-plugin/src/index.ts
+++ packages/nx-plugin/src/index.ts
@@ -1,3 +1,5 @@
 export * from './internal/versions';
 export { type InitGeneratorSchema } from './generators/init/schema';
 export { initGenerator, initSchematic } from './generators/init/generator';
+export { executeProcess, ProcessConfig } from './internal/execute-process';
+export { objectToCliArgs } from './executors/internal/cli';
