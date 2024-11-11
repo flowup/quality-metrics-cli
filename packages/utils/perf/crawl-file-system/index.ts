f9501cda3d196c354933aae036251e18f606bfe0
Michael <michael.hladky@push-based.io>
2024-11-11 17:28:25 +0100

    use mitata for benchmarking

--- packages/utils/perf/crawl-file-system/index.ts
+++ packages/utils/perf/crawl-file-system/index.ts
@@ -1,103 +1,15 @@
-import * as Benchmark from 'benchmark';
-import { join } from 'node:path';
-import {
-  type CrawlFileSystemOptions,
-  crawlFileSystem,
-} from '../../src/lib/file-system';
-import { crawlFileSystemFsWalk } from './fs-walk';
+import {bench, boxplot, run} from "mitata";
+import {crawlFileSystemFsWalk} from "./fs-walk";
+import {crawlFileSystem} from "../../src";
 
-const PROCESS_ARGUMENT_TARGET_DIRECTORY =
-  process.argv
-    .find(arg => arg.startsWith('--directory'))
-    ?.split('=')
-    .at(-1) ?? '';
-const PROCESS_ARGUMENT_PATTERN =
-  process.argv
-    .find(arg => arg.startsWith('--pattern'))
-    ?.split('=')
-    .at(-1) ?? '';
+bench('Base', () => crawlFileSystem({directory: '../../../node_modules', pattern:'test'}));
+bench('nodelib.fsWalk', () => crawlFileSystemFsWalk({directory: '../../../node_modules', pattern:'**'}));
 
-const suite = new Benchmark.Suite('report-scoring');
-
-const TARGET_DIRECTORY =
-  PROCESS_ARGUMENT_TARGET_DIRECTORY ||
-  join(process.cwd(), '..', '..', '..', 'node_modules');
-const PATTERN = PROCESS_ARGUMENT_PATTERN || /.json$/;
-
-// ==================
-
-const start = performance.now();
-
-// Add listener
-const listeners = {
-  cycle: function (event: Benchmark.Event) {
-    console.info(String(event.target));
-  },
-  complete: () => {
-    if (typeof suite.filter === 'function') {
-      console.info(' ');
-      console.info(
-        `Total Duration: ${((performance.now() - start) / 1000).toFixed(
-          2,
-        )} sec`,
-      );
-      console.info(`Fastest is ${String(suite.filter('fastest').map('name'))}`);
-    }
-  },
-};
-
-// ==================
-
-// Add tests
-const options = {
-  directory: TARGET_DIRECTORY,
-  pattern: PATTERN,
-};
-suite.add('Base', wrapWithDefer(crawlFileSystem));
-suite.add('nodelib.fsWalk', wrapWithDefer(crawlFileSystemFsWalk));
-
-// ==================
-
-// Add Listener
-Object.entries(listeners).forEach(([name, fn]) => {
-  suite.on(name, fn);
+boxplot(() => {
+  bench('new Array($size)', function* (state) {
+    const size = state.get('size');
+    yield () => Array.from({ length: size });
+  }).range('size', 1, 1024);
 });
 
-// ==================
-
-console.info('You can adjust the test with the following arguments:');
-console.info(
-  `directory      target directory of test      --directory=${TARGET_DIRECTORY}`,
-);
-console.info(
-  `pattern        pattern to search             --pattern=${PATTERN}`,
-);
-console.info(' ');
-console.info('Start benchmark...');
-console.info(' ');
-
-suite.run({
-  async: true,
-});
-
-// ==============================================================
-
-function wrapWithDefer<T>(
-  asyncFn: (options: CrawlFileSystemOptions<T>) => Promise<unknown[]>,
-) {
-  return {
-    defer: true, // important for async functions
-    fn: function (deferred: { resolve: () => void }) {
-      return asyncFn(options)
-        .catch(() => [])
-        .then((result: unknown[]) => {
-          if (result.length === 0) {
-            throw new Error(`Result length is ${result.length}`);
-          } else {
-            deferred.resolve();
-          }
-          return void 0;
-        });
-    },
-  };
-}
+await run();
--- packages/utils/perf/score-report/index.ts
+++ packages/utils/perf/score-report/index.ts
@@ -1,41 +1,42 @@
-import Benchmark from 'benchmark';
-import type { Report } from '@code-pushup/models';
-import { scoreReport } from '../../src/lib/reports/scoring';
-import { scoreReportOptimized0 } from './optimized0';
-import { scoreReportOptimized1 } from './optimized1';
-import { scoreReportOptimized2 } from './optimized2';
-import { scoreReportOptimized3 } from './optimized3';
+import {bench, summary, compact, run} from 'mitata';
+import type {Report} from '@code-pushup/models';
+import {scoreReport} from '../../src/lib/reports/scoring';
+import {scoreReportOptimized0} from './optimized0';
+import {scoreReportOptimized1} from './optimized1';
+import {scoreReportOptimized2} from './optimized2';
+import {scoreReportOptimized3} from './optimized3';
+
+// ==============================================================
 
 type MinimalReportOptions = {
-  numAuditsP1?: number;
-  numAuditsP2?: number;
-  numGroupRefs2?: number;
+    numAuditsP1?: number;
+    numAuditsP2?: number;
+    numGroupRefs2?: number;
 };
 
 const PROCESS_ARGUMENT_NUM_AUDITS_P1 = Number.parseInt(
-  process.argv
-    .find(arg => arg.startsWith('--numAudits1'))
-    ?.split('=')
-    .at(-1) ?? '0',
-  10,
+    process.argv
+        .find(arg => arg.startsWith('--numAudits1'))
+        ?.split('=')
+        .at(-1) ?? '0',
+    10,
 );
 const PROCESS_ARGUMENT_NUM_AUDITS_P2 = Number.parseInt(
-  process.argv
-    .find(arg => arg.startsWith('--numAudits2'))
-    ?.split('=')
-    .at(-1) ?? '0',
-  10,
+    process.argv
+        .find(arg => arg.startsWith('--numAudits2'))
+        ?.split('=')
+        .at(-1) ?? '0',
+    10,
 );
 const PROCESS_ARGUMENT_NUM_GROUPS_P2 = Number.parseInt(
-  process.argv
-    .find(arg => arg.startsWith('--numGroupRefs2'))
-    ?.split('=')
-    .at(-1) ?? '0',
-  10,
+    process.argv
+        .find(arg => arg.startsWith('--numGroupRefs2'))
+        ?.split('=')
+        .at(-1) ?? '0',
+    10,
 );
 
 // eslint-disable-next-line import/no-named-as-default-member
-const suite = new Benchmark.Suite('report-scoring');
 
 const AUDIT_PREFIX = 'a-';
 const GROUP_PREFIX = 'g:';
@@ -49,164 +50,101 @@ const NUM_AUDITS_P1 = PROCESS_ARGUMENT_NUM_AUDITS_P1 || 27;
 const NUM_AUDITS_P2 = PROCESS_ARGUMENT_NUM_AUDITS_P2 || 18;
 const NUM_GROUPS_P2 = PROCESS_ARGUMENT_NUM_GROUPS_P2 || NUM_AUDITS_P2 / 2;
 
-// ==================
-
-// Add listener
-const listeners = {
-  cycle: function (event: Benchmark.Event) {
-    console.info(String(event.target));
-  },
-  complete: () => {
-    if (typeof suite.filter === 'function') {
-      console.info(' ');
-      console.info(`Fastest is ${String(suite.filter('fastest').map('name'))}`);
-    }
-  },
-};
-
-// ==================
-
-// Add tests
-suite.add('scoreReport', scoreReport);
-suite.add('scoreReportOptimized0', scoreMinimalReportOptimized0);
-suite.add('scoreReportOptimized1', scoreMinimalReportOptimized1);
-suite.add('scoreReportOptimized2', scoreMinimalReportOptimized2);
-suite.add('scoreReportOptimized3', scoreMinimalReportOptimized3);
-
-// ==================
-
-// Add Listener
-Object.entries(listeners).forEach(([name, fn]) => {
-  suite.on(name, fn);
-});
-
-// ==================
-
-console.info('You can adjust the number of runs with the following arguments:');
-console.info(
-  `numAudits1      Number of audits in plugin 1.       --numAudits1=${NUM_AUDITS_P1}`,
-);
-console.info(
-  `numAudits2      Number of audits in plugin 2.       --numAudits2=${NUM_AUDITS_P2}`,
-);
-console.info(
-  `numGroupRefs2   Number of groups refs in plugin 2.  --numGroupRefs2=${NUM_GROUPS_P2}`,
-);
-console.info(' ');
-console.info('Start benchmark...');
-console.info(' ');
-
-const start = performance.now();
-
-suite.run({
-  onComplete: () => {
-    console.info(
-      `Total Duration: ${((performance.now() - start) / 1000).toFixed(2)} sec`,
-    );
-  },
-});
-
-// ==============================================================
-
-function scoreMinimalReportOptimized0() {
-  scoreReportOptimized0(minimalReport());
-}
-
-function scoreMinimalReportOptimized1() {
-  scoreReportOptimized1(minimalReport());
-}
-
-function scoreMinimalReportOptimized2() {
-  scoreReportOptimized2(minimalReport());
-}
-
-function scoreMinimalReportOptimized3() {
-  scoreReportOptimized3(minimalReport());
-}
-
-// ==============================================================
-
 // eslint-disable-next-line max-lines-per-function
 function minimalReport(opt?: MinimalReportOptions): Report {
-  const numAuditsP1 = opt?.numAuditsP1 ?? NUM_AUDITS_P1;
-  const numAuditsP2 = opt?.numAuditsP2 ?? NUM_AUDITS_P2;
-  const numGroupRefs2 = opt?.numGroupRefs2 ?? NUM_GROUPS_P2;
+    const numAuditsP1 = opt?.numAuditsP1 ?? NUM_AUDITS_P1;
+    const numAuditsP2 = opt?.numAuditsP2 ?? NUM_AUDITS_P2;
+    const numGroupRefs2 = opt?.numGroupRefs2 ?? NUM_GROUPS_P2;
 
-  const date = new Date();
+    const date = new Date();
 
-  return {
-    date: date.toISOString(),
-    packageName: 'perf-benchmark',
-    version: '0',
-    commit: {
-      date: date,
-      message: 'perf: benchmark score report',
-      author: 'me',
-      hash: 'mock_hash',
-    },
-    duration: 0,
-    categories: [
-      {
-        slug: 'c1_',
-        title: 'Category 1',
-        refs: Array.from({ length: numAuditsP1 }).map((_, idx) => ({
-          type: 'audit',
-          plugin: SLUG_PLUGIN_P1,
-          slug: `${AUDIT_P1_PREFIX}${idx}`,
-          weight: 1,
-        })),
-        isBinary: false,
-      },
-      {
-        slug: 'c2_',
-        title: 'Category 2',
-        refs: Array.from({ length: numAuditsP2 }).map((_, idx) => ({
-          type: 'audit',
-          plugin: SLUG_PLUGIN_P2,
-          slug: `${AUDIT_P2_PREFIX}${idx}`,
-          weight: 1,
-        })),
-        isBinary: false,
-      },
-    ],
-    plugins: [
-      {
-        date: '2022-01-01',
+    return {
+        date: date.toISOString(),
+        packageName: 'perf-benchmark',
+        version: '0',
+        commit: {
+            date: date,
+            message: 'perf: benchmark score report',
+            author: 'me',
+            hash: 'mock_hash',
+        },
         duration: 0,
-        slug: SLUG_PLUGIN_P1,
-        title: 'Plugin 1',
-        icon: 'slug',
-        audits: Array.from({ length: numAuditsP1 }).map((_, idx) => ({
-          value: 0,
-          slug: `${AUDIT_P1_PREFIX}${idx}`,
-          title: 'Default Title',
-          score: 0.1,
-        })),
-        groups: [],
-      },
-      {
-        date: '2022-01-01',
-        duration: 0,
-        slug: SLUG_PLUGIN_P2,
-        title: 'Plugin 2',
-        icon: 'slug',
-        audits: Array.from({ length: numAuditsP2 }).map((_, idx) => ({
-          value: 0,
-          slug: `${AUDIT_P2_PREFIX}${idx}`,
-          title: 'Default Title',
-          score: 0.1,
-        })),
-        groups: [
-          {
-            title: 'Group 1',
-            slug: GROUP_P2_PREFIX + 1,
-            refs: Array.from({ length: numGroupRefs2 }).map((_, idx) => ({
-              slug: `${AUDIT_P2_PREFIX}${idx}`,
-              weight: 1,
-            })),
-          },
+        categories: [
+            {
+                slug: 'c1_',
+                title: 'Category 1',
+                refs: Array.from({length: numAuditsP1}).map((_, idx) => ({
+                    type: 'audit',
+                    plugin: SLUG_PLUGIN_P1,
+                    slug: `${AUDIT_P1_PREFIX}${idx}`,
+                    weight: 1,
+                })),
+                isBinary: false,
+            },
+            {
+                slug: 'c2_',
+                title: 'Category 2',
+                refs: Array.from({length: numAuditsP2}).map((_, idx) => ({
+                    type: 'audit',
+                    plugin: SLUG_PLUGIN_P2,
+                    slug: `${AUDIT_P2_PREFIX}${idx}`,
+                    weight: 1,
+                })),
+                isBinary: false,
+            },
         ],
-      },
-    ],
-  };
+        plugins: [
+            {
+                date: '2022-01-01',
+                duration: 0,
+                slug: SLUG_PLUGIN_P1,
+                title: 'Plugin 1',
+                icon: 'slug',
+                audits: Array.from({length: numAuditsP1}).map((_, idx) => ({
+                    value: 0,
+                    slug: `${AUDIT_P1_PREFIX}${idx}`,
+                    title: 'Default Title',
+                    score: 0.1,
+                })),
+                groups: [],
+            },
+            {
+                date: '2022-01-01',
+                duration: 0,
+                slug: SLUG_PLUGIN_P2,
+                title: 'Plugin 2',
+                icon: 'slug',
+                audits: Array.from({length: numAuditsP2}).map((_, idx) => ({
+                    value: 0,
+                    slug: `${AUDIT_P2_PREFIX}${idx}`,
+                    title: 'Default Title',
+                    score: 0.1,
+                })),
+                groups: [
+                    {
+                        title: 'Group 1',
+                        slug: GROUP_P2_PREFIX + 1,
+                        refs: Array.from({length: numGroupRefs2}).map((_, idx) => ({
+                            slug: `${AUDIT_P2_PREFIX}${idx}`,
+                            weight: 1,
+                        })),
+                    },
+                ],
+            },
+        ],
+    };
 }
+
+// ======================================
+
+// Add tests
+summary(() => {
+   // compact(() => {
+        bench('scoreReportCurrentImplementation', () => scoreReport(minimalReport()));
+        bench('scoreReportOptimized0', () => scoreReportOptimized0(minimalReport()));
+        bench('scoreReportOptimized1', () => scoreReportOptimized1(minimalReport()));
+        bench('scoreReportOptimized2', () => scoreReportOptimized2(minimalReport()));
+        bench('scoreReportOptimized3', () => scoreReportOptimized3(minimalReport()));
+    //})
+})
+await run();
